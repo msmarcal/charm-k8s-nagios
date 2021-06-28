15fdc4df263d6c3c87858d6362dd2b71623e9c62
Marcelo Marcal <marcelo.marcal@canonical.com>
2021-06-28 11:48:23 -0300

    Add relation config

--- src/charm.py
+++ src/charm.py
@@ -11,6 +11,7 @@ from ops.framework import StoredState
 from ops.framework import EventBase
 from ops.main import main
 from ops.model import ActiveStatus
+from ops.pebble import ServiceStatus
 
 from pynag import Model
 
@@ -18,7 +19,7 @@ logger = logging.getLogger(__name__)
 
 MAIN_NAGIOS_DIR = "/etc/nagios4"
 MAIN_NAGIOS_CFG = "/etc/nagios4/nagios.cfg"
-CHARM_CFG = "/etc/nagios4/conf.d/charm.cfg"
+CHARM_CFG_PATH = "/etc/nagios4/conf.d"
 PLUGIN_PATH = "/usr/lib/nagios/plugins"
 EXTRA_CFG = "/etc/nagios4/conf.d/extra.cfg"
 
@@ -29,12 +30,15 @@ class K8SNagiosCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
         self.framework.observe(self.on.nagios_pebble_ready, self._on_nagios_pebble_ready)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
+
+        # -- config changed observation
+        # self.framework.observe(self.on.config_changed, self._on_config_changed)
 
         # -- monitors relation observation
         self.framework.observe(self.on["monitors"].relation_changed, self._on_monitors_changed)
+        self.framework.observe(self.on["monitors"].relation_departed, self._on_monitors_departed)
 
-        self._stored.set_default(extraconfig=[])
+        # self._stored.set_default(extraconfig=[])
 
     def _on_nagios_pebble_ready(self, event):
         """Define and start a workload using the Pebble API."""
@@ -45,9 +49,9 @@ class K8SNagiosCharm(CharmBase):
             "summary": "nagios layer",
             "description": "pebble config layer for nagios",
             "services": {
-                "nagios": {
+                "nagiossvc": {
                     "override": "replace",
-                    "summary": "nagios",
+                    "summary": "nagios service",
                     "command": "/usr/sbin/nagios4 /etc/nagios4/nagios.cfg",
                     "startup": "enabled",
                     "environment": {
@@ -63,52 +67,102 @@ class K8SNagiosCharm(CharmBase):
         self.unit.status = ActiveStatus()
 
 
-    def _on_config_changed(self, _):
-        """Changed configuration."""
+    def _restart_nagios(self):
         container = self.unit.get_container("nagios")
-        current = self.config["extraconfig"]
-        if current not in self._stored.extraconfig:
-            logger.info("found a new extraconfig: [%r]", current)
-            self._stored.extraconfig.append(current)
-            container.push(EXTRA_CFG, current)
-        if current == "" and os.path.isfile(EXTRA_CFG):
-            logger.info("Removing {}...".format(EXTRA_CFG))
-            container.remove_path(EXTRA_CFG)
+
+        try:
+            status = container.get_service("nagiossvc")
+        except:
+            return
+
+        if status.current == ServiceStatus.ACTIVE:
+            container.stop("nagiossvc")
+
+        container.start("nagiossvc")
+
+
+
+    # def _on_config_changed(self, _):
+    #     """Changed extraconfig configuration."""
+    #     container = self.unit.get_container("nagios")
+    #     current = self.config["extraconfig"]
+    #     if current not in self._stored.extraconfig:
+    #         logger.info("found a new extraconfig: [%r]", current)
+    #         self._stored.extraconfig.append(current)
+    #         container.push(EXTRA_CFG, current)
+    #     if current == "" and os.path.isfile(EXTRA_CFG):
+    #         logger.info("Removing {}...".format(EXTRA_CFG))
+    #         container.remove_path(EXTRA_CFG)
 
 
     def _on_monitors_changed(self, event: EventBase):
         """Changed monitors relation"""
         container = self.unit.get_container("nagios")
         remote_data = event.relation.data[event.unit]
-        hostname = remote_data['target-id']
+
+        try:
+            hostname = remote_data['target-id']
+        except KeyError:
+            return
+
+        host_filename = CHARM_CFG_PATH + "/{}.cfg".format(hostname)
 
         # Add the new host
         host = Model.Host()
-        host.set_filename(CHARM_CFG)
+        host.set_filename(host_filename)
         host.set_attribute("host_name", hostname)
         host.set_attribute("use", "generic-host")
+        host.set_attribute("address", remote_data['ingress-address'])
+        host.set_attribute("max_check_attempts", 5)
+        host.set_attribute("check_period", "24x7")
+        host.set_attribute("contact_groups", "admins")
+        host.set_attribute("notification_options", "d,u,r")
+        host.set_attribute("notification_interval", 30)
+        host.set_attribute("notification_period", "24x7")
         # Adding the ubuntu icon image definitions to the host.
         host.set_attribute("icon_image", "base/ubuntu.png")
         host.set_attribute("icon_image_alt", "Ubuntu Linux")
         host.set_attribute("vrml_image", "ubuntu.png")
         host.set_attribute("statusmap_image", "base/ubuntu.gd2")
-        host.set_attribute("address", remote_data['ingress-address'])
 
         # Add the services
         checks = json.loads(remote_data['monitors'].replace("'", "\""))
-
         services = ""
         service = Model.Service()
-        service.set_filename(CHARM_CFG)
+        service.set_filename(host_filename)
+
         for check in checks['monitors']['remote']['nrpe'].values():
             service.set_attribute("host_name", hostname)
             service.set_attribute("use", "generic-service")
-            service.set_attribute("check_command", "check_nrpe_" + check)
+            service.set_attribute("check_command", "nrpe_" + check)
+            service.set_attribute("service_description", "nrpe_" + check)
             services += str(service)
 
-        # Save the config
-        container.push(CHARM_CFG, str(host) + str(services))
+        # Save the config file
+        logger.info("Adding config for {} host".format(hostname))
+        container.push(host_filename, str(host) + services)
 
+        self._restart_nagios()
+
+
+    def _on_monitors_departed(self, event: EventBase):
+        """ Monitors relation departed. """
+        container = self.unit.get_container("nagios")
+        # remote_data = event.relation.data[event.unit]
+        remote_data = event.relation.data[event.app]
+        logger.info
+
+        try:
+            hostname = remote_data['target-id']
+            logger.info("----- target-id = {}".format(hostname))
+        except KeyError:
+            return
+
+        # host_filename = CHARM_CFG_PATH + "/{}.cfg".format(hostname)
+        # logger.info("Removing {} configuration.".format(hostname))
+        # container.remove_path(host_filename)
+
+        self._restart_nagios()
 
 if __name__ == "__main__":
     main(K8SNagiosCharm)
